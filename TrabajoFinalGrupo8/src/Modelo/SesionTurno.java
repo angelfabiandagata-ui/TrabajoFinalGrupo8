e870a7eeeae7333b204262eb5b4a795ece835d62
Nico-ceo <teito.bris@gmail.com>
2025-10-21 19:18:45 -0300

    clases diadespa y  sesionturno

---
+++ TrabajoFinalGrupo8/src/Modelo/SesionTurno.java
@@ -0,0 +1,106 @@
+
+package Modelo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class SesionTurno {
+    
+    private int codSesion;
+    private LocalDateTime fechaHoraInicio;
+    private LocalDateTime fechaHoraFin;
+    private String tratamiento; 
+    private String consultorio;
+    private String masajista;
+    private List<Instalacion>instalacion;
+
+
+
+/*
+
+    public SesionTurno(int codSesion, LocalDateTime fechaHoraInicio, LocalDateTime fechaHoraFin, String tratamiento, String consultorio, String masajista, List<Instalacion> instalaciones, LocalDate diaDeSpa) {
+        this.codSesion = codSesion;
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.tratamiento = tratamiento;
+        this.consultorio = consultorio;
+        this.masajista = masajista;
+        this.instalaciones = instalaciones;
+        this.diaDeSpa = diaDeSpa;
+    }
+
+    public int getCodSesion() {
+        return codSesion;
+    }
+
+    public void setCodSesion(int codSesion) {
+        this.codSesion = codSesion;
+    }
+
+    public LocalDateTime getFechaHoraInicio() {
+        return fechaHoraInicio;
+    }
+
+    public void setFechaHoraInicio(LocalDateTime fechaHoraInicio) {
+        this.fechaHoraInicio = fechaHoraInicio;
+    }
+
+    public LocalDateTime getFechaHoraFin() {
+        return fechaHoraFin;
+    }
+
+    public void setFechaHoraFin(LocalDateTime fechaHoraFin) {
+        this.fechaHoraFin = fechaHoraFin;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public String getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(String consultorio) {
+        this.consultorio = consultorio;
+    }
+
+    public String getMasajista() {
+        return masajista;
+    }
+
+    public void setMasajista(String masajista) {
+        this.masajista = masajista;
+    }
+
+    public List<Instalacion> getInstalacioneslist() {
+        return instalaciones;
+    }
+
+    public void setInstalacioneslist(List<Instalacion> instalacioneslist) {
+        this.instalaciones = instalacioneslist;
+    }
+
+    public LocalDate getDiaDeSpa() {
+        return diaDeSpa;
+    }
+
+    public void setDiaDeSpa(LocalDate diaDeSpa) {
+        this.diaDeSpa = diaDeSpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Sesion-Turno" + "codSesion=" + codSesion + " fechaHoraInicio=" + fechaHoraInicio + " fechaHoraFin = "  + fechaHoraFin + " tratamiento = " + tratamiento + " consultorio = " + consultorio + " masajista = " + masajista + " instalaciones = " + instalaciones + "  diaDeSpa = " + diaDeSpa ;
+    }
+     
+            
+    
+}*/
+
