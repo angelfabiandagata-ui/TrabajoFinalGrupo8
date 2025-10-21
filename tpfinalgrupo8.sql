7ccbc171ebdecff5555d9b501577e3f9eed99b61
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-10-21 13:31:23 -0300

    Base de datos Base
    base de base de la base :v

---
+++ tpfinalgrupo8.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-10-2025 a las 18:30:32
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tpfinalgrupo8`
+--
+CREATE DATABASE IF NOT EXISTS `tpfinalgrupo8` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tpfinalgrupo8`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entidad`
+--
+
+CREATE TABLE `entidad` (
+  `atributouno` int(11) NOT NULL,
+  `atributodos` int(11) NOT NULL,
+  `atributotres` int(11) NOT NULL,
+  `atributocuatro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
