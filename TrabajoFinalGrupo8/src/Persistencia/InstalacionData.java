31af9b80484038563086946f665f0f8f4574ed91
Lucas <serranolucas1000@gmail.com>
2025-10-21 19:07:09 -0300

    Se comentan los metodos

--- TrabajoFinalGrupo8/src/Persistencia/InstalacionData.java
+++ TrabajoFinalGrupo8/src/Persistencia/InstalacionData.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Persistencia;
 
-/**
- *
- * @author user
- */
+import Modelo.Conexion;
+import java.sql.Connection;
+import java.util.List;
+
 public class InstalacionData {
-    
+
+    private Connection con = null;
+
+    public InstalacionData(java.sql.Connection conexion) {
+        this.con = conexion;
+    }
+
+    public InstalacionData() {
+        String url = "localhost/phpmyadmin/index.php?route=database/structure&db=sgulp_equipo_8";
+        String usuario = "root";
+        String password = "1234";
+
+        try {
+            Conexion conAux = new Conexion(url, usuario, password);
+            this.con = conAux.buscarConexion();
+        } catch (Exception e) {
+            System.err.print("Error");
+        }
+
+    }
+
+//    public void AltaInstalacion(){
+//        
+//    }
+//    public void BajaInstalacion(){
+//        
+//    }
+//    public void ModificarInstalacion(){
+//        
+//    }
+//    public void CrearSesionIns() {
+//
+//    }
+//
+//    public List<Instalacion> ListarInstalaciones() {
+//
+//    }
+//
+//    public List<Instalacion> ListarInstalacionesMasSolicitadas() {
+//
+//    }
 }
--- TrabajoFinalGrupo8/src/Persistencia/TratamientoData.java
+++ TrabajoFinalGrupo8/src/Persistencia/TratamientoData.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Persistencia;
 
-/**
- *
- * @author user
- */
+import Modelo.Conexion;
+import Modelo.Tratamiento;
+import java.sql.Connection;
+import java.util.List;
+
 public class TratamientoData {
+
+    private Connection con = null;
+
+    public TratamientoData(java.sql.Connection conexion) {
+        this.con = conexion;
+    }
+
+    public TratamientoData() {
+        String url = "localhost/phpmyadmin/index.php?route=database/structure&db=sgulp_equipo_8";
+        String usuario = "root";
+        String password = "1234";
+
+        try {
+            Conexion conAux = new Conexion(url, usuario, password);
+            this.con = conAux.buscarConexion();
+        } catch (Exception e) {
+            System.err.print("Error");
+        }
+
+    }
+    
+//    public void AltaTratamiento(){
+//        
+//    }
+//    public void BajaTratamiento(){
+//        
+//    }
+//    public void ModificarTratamiento(){
+//        
+//    }
+//    public List<Tratamiento> ListarTratamiento(){
+//        
+//    }
+//     public List<Tratamiento> TratamientosMasSesionados(){
+//        
+//    }
     
 }
