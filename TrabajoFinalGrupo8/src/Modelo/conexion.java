3b43efa01e00cbf9a49b80bde635f284a16de677
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-10-21 13:12:52 -0300

    conexion clase
    clase conexion

--- TrabajoFinalGrupo8/src/Modelo/conexion.java
+++ TrabajoFinalGrupo8/src/Modelo/conexion.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
-/**
- *
- * @author angel
- */
-public class conexion {
-    
+import java.sql.Connection;
+import org.mariadb.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+
+    private String url;
+    private String usuario;
+    private String password;
+
+    private static Connection conexion = null;
+
+    public Conexion(String url, String usuario, String password) {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public Connection buscarConexion() {
+        if (conexion == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(url, usuario, password);
+                
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "No se pudo cargar el driver" + ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"No se puede conectar" + ex.getMessage());
+
+            }
+
+        }
+        return conexion;
+    }
+
 }
