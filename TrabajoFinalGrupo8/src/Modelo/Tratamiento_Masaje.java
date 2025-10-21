2e4d2e6dd04b39e0b755c4eb7b4e4f0b62f6f7e4
Lucas <serranolucas1000@gmail.com>
2025-10-21 18:35:38 -0300

    Creacion clase tratamiento

---
+++ TrabajoFinalGrupo8/src/Modelo/Tratamiento_Masaje.java
@@ -0,0 +1,86 @@
+
+package Modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Tratamiento_Masaje {
+    private int codTratam; 
+    private String nombre;
+    private String detalle;
+    private List<String> Productos;
+    private int Duracion; //Minutos
+    private double costo;
+    private boolean activo;
+
+    public Tratamiento_Masaje() {
+    }
+
+    public Tratamiento_Masaje(int codTratam, String nombre, String detalle, int Duracion, double costo, boolean activo) {
+        this.codTratam = codTratam;
+        this.nombre = nombre;
+        this.detalle = detalle;
+        this.Duracion = Duracion;
+        this.costo = costo;
+        this.activo = activo;
+    }
+
+    public int getCodTratam() {
+        return codTratam;
+    }
+
+    public void setCodTratam(int codTratam) {
+        this.codTratam = codTratam;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public List<String> getProductos() {
+        return Productos;
+    }
+
+    public void setProductos(List<String> Productos) {
+        this.Productos = Productos;
+    }
+
+    public int getDuracion() {
+        return Duracion;
+    }
+
+    public void setDuracion(int Duracion) {
+        this.Duracion = Duracion;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+   
+}
