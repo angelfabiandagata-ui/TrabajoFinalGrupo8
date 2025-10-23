45e19ff7333726e384fde8af044c2eadd0b6ddf8
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-10-23 20:02:25 -0300

    InstalacionData
    instalacion data terminada
    falta probar

--- TrabajoFinalGrupo8/src/Persistencia/InstalacionData.java
+++ TrabajoFinalGrupo8/src/Persistencia/InstalacionData.java
@@ -1,12 +1,15 @@
 package Persistencia;
 
 import Modelo.Conexion;
+import Modelo.Instalacion;
 import Modelo.SesionTurno;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Statement;
 
 public class InstalacionData {
@@ -30,22 +33,154 @@ public class InstalacionData {
         }
 
     }
-
-//    public void AltaInstalacion(){
-//        
-//    }
-//    public void BajaInstalacion(){
-//        
-//    }
-//    public void ModificarInstalacion(){
-//        
-//    }
-//
-//    public List<Instalacion> ListarInstalaciones() {
-//
-//    }
-//
 //    public List<Instalacion> ListarInstalacionesMasSolicitadas() {
-//
 //    }
+    
+    public void guardarInstalacion(Instalacion inst) { 
+    
+    String sql = "INSERT INTO `instalacion`( `nombre`, `detalle_uso`, `precio30m`, `estado`) VALUES (?,?,?,?,?)";
+    try {
+        
+        PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, inst.getNombre());
+        ps.setString(2, inst.getDetalleUso());
+        ps.setDouble(3, inst.getPrecio30min());
+        ps.setBoolean(4, inst.isEstado());
+
+        int filas = ps.executeUpdate();
+
+        if (filas > 0) {
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                inst.setCodInstal(rs.getInt(0));
+            }
+            JOptionPane.showMessageDialog(null, " Instalacion guardada correctamente");
+        } else {
+            JOptionPane.showMessageDialog(null, "️ No se insertó ningúna Instalacion");
+        }
+
+        ps.close();
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "❌ Error al inscribir: " + ex.getMessage());
+    }
+}
+
+    public Instalacion buscarInstalacion(int id) {
+       
+        String sql = "SELECT * FROM `instalacion` WHERE id = ?";
+        Instalacion inst = null; 
+    
+    try {
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) { 
+            inst = new Instalacion(); 
+            
+            inst.setCodInstal(rs.getInt(0));
+            inst.setNombre(rs.getString(1));
+            inst.setDetalleUso(rs.getString(2));
+            inst.setPrecio30min(rs.getDouble(3));
+            inst.setEstado(rs.getBoolean(4));                  
+            rs.close();
+            ps.close();
+        }           
+    } catch (SQLException e) {
+        System.err.println("Error al buscar el alumno: " + e.getMessage());
+    }
+    return inst; 
+    }
+    
+    public void actualizarInstalacion(Instalacion inst) {
+        try {
+            String sql = "UPDATE `instalacion` SET `codInstalacion`=?,`nombre`=?,`detalle_uso`=?,`precio30m`=?,`estado`=? WHERE 1";
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, inst.getCodInstal());
+            ps.setString(2, inst.getNombre());
+            ps.setString(3, inst.getDetalleUso());
+            ps.setDouble(4, inst.getPrecio30min());
+            ps.setBoolean(5, inst.isEstado());
+            
+            ps.executeUpdate();
+            ps.close();
+            System.out.println("Alumno actualizado correctamente!!");
+        } catch (SQLException ex) {
+            System.out.println("Error al actualizar el alumno" + ex.getMessage()
+            );
+        }
+
+    }
+    
+    public void borrarInstalacion(int cod) {
+        try {
+            String sql = "DELETE FROM `instalacion` WHERE codInstalacion = ? ";
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, cod);
+            ps.executeUpdate();
+            ps.close();
+            System.out.println("Alumno borrado con exito");
+        } catch (SQLException ex) {
+            System.out.println("Error al borrar el alumno" + ex.getMessage());
+        }
+
+    }
+
+     public List<Instalacion> listarInstalasciones() {
+        List<Instalacion> instalaciones = new ArrayList<>();
+        String sql = "SELECT * FROM `alumno` WHERE `estado` = true";
+        
+          try {
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            Instalacion inst = new Instalacion();    
+            
+            inst.setCodInstal(rs.getInt(0));
+            inst.setNombre(rs.getString(1));
+            inst.setDetalleUso(rs.getString(2));
+            inst.setPrecio30min(rs.getDouble(3));
+            inst.setEstado(rs.getBoolean(4)); 
+                       
+            inst.setEstado(true);
+            instalaciones.add(inst);
+        }
+          ps.close();
+    } catch (SQLException ex) {
+        System.out.println("Error al listar alumnos: " + ex.getMessage());
+    }
+    return instalaciones;
+
+    }
+     public void altaLogica(int cod) {
+        try {
+            String sql = "UPDATE `instalacion` SET `estado`='1' WHERE `codInstalacion` = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, cod);
+            ps.executeUpdate();
+            ps.close();
+            System.out.println("Alumno dado de alta correctamente!!");
+        } catch (SQLException ex) {
+            System.out.println("Error al dar de alta el alumno" + ex.getMessage()
+            );
+        }
+    }
+    public void bahaLogica(int cod) {
+        try {
+            String sql = "UPDATE `instalacion` SET `estado`='0' WHERE `codInstalacion` = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, cod);
+            ps.executeUpdate();
+            ps.close();
+            System.out.println("Alumno dado de alta correctamente!!");
+        } catch (SQLException ex) {
+            System.out.println("Error al dar de alta el alumno" + ex.getMessage()
+            );
+        }
+    }
+        
+//final    
 }
