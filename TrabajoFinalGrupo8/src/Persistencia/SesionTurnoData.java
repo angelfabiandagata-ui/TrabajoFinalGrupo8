1ff6761285a3faeec9df698f89f1ec05ccca7d60
Lucas <serranolucas1000@gmail.com>
2025-10-28 19:48:12 -0300

    Primeros metodos clase sesionturnoData
    solo se crean los dos primero metodos, no probados aun

--- TrabajoFinalGrupo8/src/Persistencia/SesionTurnoData.java
+++ TrabajoFinalGrupo8/src/Persistencia/SesionTurnoData.java
@@ -1,25 +1,97 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Persistencia;
 
+import Modelo.Conexion;
+import Modelo.Instalacion;
 import Modelo.SesionTurno;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-/**
- *
- * @author DAIAN
- */
 public class SesionTurnoData {
     
-    
+      private Connection con = null;
+
+    public SesionTurnoData(java.sql.Connection conexion) {
+        this.con = conexion;
+    }
+
+    public SesionTurnoData() {
+        String url = "localhost/phpmyadmin/index.php?route=database/structure&db=sgulp_equipo_8";
+        String usuario = "root";
+        String password = "1234";
+
+        try {
+            Conexion conAux = new Conexion(url, usuario, password);
+            this.con = conAux.buscarConexion();
+        } catch (Exception e) {
+            System.err.print("Error");
+        }
+
+    }
     
     public void crearSesion(SesionTurno sesionturno){
-    
+    String sql = "INSERT INTO `sesion`(`codSesion`, `fechaHoraInicio`, `fechaHoraFin`, `codTratamiento`, `nroConsultorio`, `matricula`, `codInstalacion`, `codPack`, `estado`) VALUES "
+            + "(?,?,?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, sesionturno.getCodSesion());
+            ps.setTime(2, java.sql.Time.valueOf(sesionturno.getFechaHoraInicio()));
+            ps.setTime(3, java.sql.Time.valueOf(sesionturno.getFechaHoraFin()));
+            ps.setInt(4, sesionturno.getTratamiento().getCodTratam());
+            ps.setInt(5, sesionturno.getConsultorio().getNroConsultorio());
+            ps.setLong(6, sesionturno.getMasajista().getMatricula());
+            ps.setArray(7, (Array) sesionturno.getInstalacionesList());
+            ps.setDate(8, java.sql.Date.valueOf(sesionturno.getDiaDeSpa()));
+            ps.setBoolean(9, sesionturno.getEstado());
+            
+            ps.executeUpdate();
+            ps.close();
+            System.out.println("Sesion creada correctamente");
+            
+        } catch (SQLException ex) {
+            System.out.println("Error al crear excepcion" +ex.getMessage());
+        }
 }
    public void Modificar (SesionTurno sesionturno){
+       String sql = "UPDATE `sesion` SET `fechaHoraInicio`=?,`fechaHoraFin`=?,`codTratamiento`=?,`nroConsultorio`=?,`matricula`=?,estado=? WHERE codSesion = ?";
+       String sqlDelete = "DELETE FROM sesion WHERE codSesion = ?";
+    String sqlInsert = "INSERT INTO sesion (codSesion, codInstalacion) VALUES (?, ?)";
+       
+           try {
+               
+               PreparedStatement ps =con.prepareStatement(sql);
+               PreparedStatement psDelete = con.prepareStatement(sqlDelete);
+               PreparedStatement psInsert = con.prepareStatement(sqlInsert);
+               ps.setTime(1, java.sql.Time.valueOf(sesionturno.getFechaHoraInicio()));
+               ps.setTime(2, java.sql.Time.valueOf(sesionturno.getFechaHoraFin()));
+               ps.setInt(3, sesionturno.getTratamiento().getCodTratam());
+               ps.setInt(4, sesionturno.getConsultorio().getNroConsultorio());
+               ps.setLong(5, sesionturno.getMasajista().getMatricula());
+               ps.setBoolean(6, sesionturno.getEstado());
+               ps.setInt(7, sesionturno.getCodSesion());
+               
+               ps.executeUpdate();
+               
+               psDelete.setInt(1, sesionturno.getCodSesion());
+               psDelete.executeUpdate();
+               
+               for (Instalacion  inst : sesionturno.getInstalacionesList()) {
+                   psInsert.setInt(1, sesionturno.getCodSesion());
+                   psInsert.setInt(2, inst.getCodInstal());
+                   psInsert.executeUpdate();
+                   
+               }
+               
+               System.out.println("Masajista correctamente modificado :)");
+           } catch (SQLException ex) {
+               System.out.println("Error al crear masajsita" + ex.getMessage());
+            
+           }
+       }
     
-}
+
    
       public void ListarSesionesXDia(SesionTurno sesionturno){
     
